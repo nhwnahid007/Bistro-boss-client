ac87db4be6f9873daed38da197744e334a45e406
nhwnahid007 <nhwnahid@gmail.com>
2024-05-28 01:42:28 +0600

    email isexist post to database

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,44 +1,67 @@
-import { FaCalendar, FaHome, FaShoppingCart } from "react-icons/fa";
+import { FaCalendar, FaEnvelope, FaHome, FaShoppingCart } from "react-icons/fa";
 import { FaList } from "react-icons/fa6";
 import { MdRestaurantMenu, MdReviews } from "react-icons/md";
 import { NavLink, Outlet } from "react-router-dom";
 import useCart from "../Hooks/UseCart";
 
 const Dashboard = () => {
-  const [cart] = useCart()
+  const [cart] = useCart();
+
+  //todo: get is admin value from the database
+  const isAdmin = true;
+
   return (
     <div className="flex">
       {/* Dashboard Sidebar */}
       <div className="w-64 min-h-screen bg-orange-400">
         <ul className="menu">
           <li>
-            
-            <NavLink to="/dashboard/userHome"> <FaHome></FaHome> User Home</NavLink>
+            <NavLink to="/dashboard/userHome">
+              {" "}
+              <FaHome></FaHome> User Home
+            </NavLink>
           </li>
           <li>
-            
-            <NavLink to="/dashboard/reservation"> <FaCalendar></FaCalendar> Reservation</NavLink>
+            <NavLink to="/dashboard/reservation">
+              {" "}
+              <FaCalendar></FaCalendar> Reservation
+            </NavLink>
           </li>
           <li>
-            
-            <NavLink to="/dashboard/cart"> <FaShoppingCart></FaShoppingCart> My cart ({cart.length}) </NavLink>
+            <NavLink to="/dashboard/cart">
+              {" "}
+              <FaShoppingCart></FaShoppingCart> My cart ({cart.length}){" "}
+            </NavLink>
           </li>
           <li>
-            
-            <NavLink to="/dashboard/review"> <MdReviews /> Add a Review</NavLink>
+            <NavLink to="/dashboard/review">
+              {" "}
+              <MdReviews /> Add a Review
+            </NavLink>
           </li>
           <li>
-            
-            <NavLink to="/dashboard/bookings"> <FaList></FaList> Bookings</NavLink>
+            <NavLink to="/dashboard/bookings">
+              {" "}
+              <FaList></FaList> Bookings
+            </NavLink>
           </li>
           <div className="divider"></div>
           <li>
-            
-            <NavLink to="/"> <FaHome></FaHome>Home</NavLink>
+            <NavLink to="/">
+              {" "}
+              <FaHome></FaHome>Home
+            </NavLink>
           </li>
           <li>
-            
-            <NavLink to="/"><MdRestaurantMenu />  Menu</NavLink>
+            <NavLink to="/">
+              <MdRestaurantMenu /> Menu
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/">
+              {" "}
+              <FaEnvelope /> Contact
+            </NavLink>
           </li>
         </ul>
       </div>
@@ -46,7 +69,7 @@ const Dashboard = () => {
       <div className="flex-1 p-8">
         <Outlet></Outlet>
       </div>
-    de</div>
+    </div>
   );
 };
 
--- src/components/SocialLogin/SocialLogin.jsx
+++ src/components/SocialLogin/SocialLogin.jsx
@@ -2,29 +2,43 @@ import { FaGoogle } from "react-icons/fa";
 import UseAuth from "../../Hooks/UseAuth";
 import toast from "react-hot-toast";
 import { useLocation, useNavigate } from "react-router-dom";
+import UseAxiosPublic from "../../Hooks/UseAxiosPublic";
 
 const SocialLogin = () => {
-    const navigate  = useNavigate()
-    const location = useLocation()
-    const from = location.state?.from?.pathname || "/";
-    const {googleSignIn} = UseAuth()
-    const handleGoogleSignIn = ()=>{
-        googleSignIn()
-        .then(result=>{
-            console.log(result.user)
-            toast.success('Signed in successfully')
+  const axiosPublic = UseAxiosPublic();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const from = location.state?.from?.pathname || "/";
+  const { googleSignIn } = UseAuth();
+  const handleGoogleSignIn = () => {
+    googleSignIn()
+      .then((result) => {
+        console.log(result.user);
+        const userInfo = {
+          email: result.user?.email,
+          name: result.user?.displayName,
+        };
+        axiosPublic.post('/users',userInfo)
+        .then(res=>{
+            console.log(res.data)
+            toast.success("Signed in successfully");
             navigate(from, { replace: true });
         })
-        .catch(error=>{
-            console.log(error)
-            toast.error(`Google Sign in failed ${error}`)
-        })
-    }
+       
+      })
+      .catch((error) => {
+        console.log(error);
+        toast.error(`Google Sign in failed ${error}`);
+      });
+  };
   return (
     <div>
       <div className="mx-8">
-        <button onClick={handleGoogleSignIn} className="btn w-full flex items-center">
-         <FaGoogle></FaGoogle>
+        <button
+          onClick={handleGoogleSignIn}
+          className="btn w-full flex items-center"
+        >
+          <FaGoogle></FaGoogle>
           Sign in witn Google
         </button>
       </div>
